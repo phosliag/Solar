c91033eeb50d76cda73577ab3616e1131cb0c122
Fernando Lopez <cainuriel@gmail.com>
2025-07-24 10:23:35 +0200

    start new contracts solar

--- smartContracts/SmartAccounts/contracts/Account.sol
+++ smartContracts/SmartAccounts/contracts/Account.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.20;
 
 import {ERC20Account} from "./ERC20/ERC20Account.sol";
+import {ERC721Account} from "./ERC721/ERC721Account.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20Burnable} from "./ERC20/IERC20Account.sol";
 
-contract Account is ERC20Account, Ownable {
+contract Account is ERC20Account, ERC721Account, Ownable {
     constructor(address owner_) Ownable(owner_) {}
 
     function approveERC20(
@@ -31,4 +33,23 @@ contract Account is ERC20Account, Ownable {
     ) public override onlyOwner returns (bool) {
         return ERC20Account.burnERC20(tokenContract, amount);
     }
+
+    function transferERC721(
+        IERC721 tokenContract,
+        address to,
+        uint256 tokenId
+    ) public override onlyOwner returns (bool) {
+        return ERC721Account.transferERC721(tokenContract, to, tokenId);
+    }
+
+    function setApprovalForAllERC721(
+        IERC721 tokenContract,
+        address operator,
+        bool approved
+    ) public override onlyOwner returns (bool) {
+        return ERC721Account.setApprovalForAllERC721(tokenContract, operator, approved);
+    }
+
+
+
 }
